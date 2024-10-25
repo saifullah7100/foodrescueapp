26dec9938eb98f5003eae233e05235c030870963
Saif Ullah <ladlabhatti8240@gmail.com>
2024-10-25 19:28:38 +0500

    Delete lib directory

--- lib/Utils/customtheme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class Customtheme {
- static final lighttheme =ThemeData(
-   scaffoldBackgroundColor: const Color(0xFFACFFAC),
-  );
- static final darktheme =ThemeData(
-   scaffoldBackgroundColor: const Color(0xFFACFFAC),
-  );
-}
